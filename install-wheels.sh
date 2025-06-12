8b6aae1b20af66de27841e35e1e1f138df7534f3
Zebreus <zebreus@zebre.us>
2025-06-12 11:00:00 +0200

    Add an option to install to a different pythonhome

--- install-wheels.sh
+++ install-wheels.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 find ./wheels -name *.whl | \
-  xargs -I {} unzip -o {} -d ../cpython-install/cpython/lib/python3.13/site-packages
+  xargs -I {} unzip -o {} -d "${PYTHONHOME:=../cpython-install/cpython}/lib/python3.13/site-packages"
